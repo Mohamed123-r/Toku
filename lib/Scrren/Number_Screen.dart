a1aa8499caf6489efbbdcd2af5ae79dfbaaf940b
Mohamed Attalla <mezoattalla123@gmail.com>
2023-09-07 10:27:47 +0300

    hi

---
+++ lib/Scrren/Number_Screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Numbrer extends StatelessWidget {
+  const Numbrer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
